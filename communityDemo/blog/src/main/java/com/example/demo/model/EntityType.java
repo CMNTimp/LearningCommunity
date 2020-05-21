74e9898f598d552136096074d2a04ee9deab777d
ChestnutGohan&Kamekameha <627942458@qq.com>
2020-05-21 12:15:45 +0800

    Create EntityType.java

---
+++ communityDemo/blog/src/main/java/com/example/demo/model/EntityType.java
@@ -0,0 +1,7 @@
+package com.example.demo.model;
+
+//article的type定义为1，comment的type定义为2
+public class EntityType {
+    public static int ENTITY_ARTICLE = 1;
+    public static int ENTITY_COMMENT = 2;
+}
